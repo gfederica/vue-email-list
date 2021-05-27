ec058ce5b277a8c9efa1b73b8e0a61e43aea32be
gfederica <effegiudice@gmail.com>
2021-05-27 16:38:50 +0200

    Completato + bonus

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      emailAddress: []
+    },
+    mounted: function () {
+        for (i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then ((result) => {
+                    this.emailAddress.push(result.data.response);
+            })  
+        }
+
+    }
+  })
